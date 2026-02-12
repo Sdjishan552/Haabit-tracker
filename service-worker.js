3183196e3938fa0213a390c9578e6fa922801c9b
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-02-12 10:47:37 +0530

    Update service worker to version 3 and simplify code

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,13 @@
-const CACHE_NAME = "discipline-tracker-v2"; // ← change version when updating
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./notifications.html",
-  "./admin.html",
-  "./history.html",
-  "./stats.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
-];
+const CACHE_NAME = "discipline-tracker-v3";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
+      return Promise.all(keys.map(key => caches.delete(key)));
     })
   );
   return self.clients.claim();
