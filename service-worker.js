98dc9abb260838d0588e3ded5509fd9c02447bad
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-02-12 16:04:40 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "discipline-tracker-JARVIS v1.7";self.addEventListener("install", event => {
+const CACHE_NAME = "discipline-tracker-JARVIS v1.8";self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
@@ -17,6 +17,7 @@ self.addEventListener("fetch", event => {
       .catch(() => caches.match(event.request))
   );
 });
+
 
 
 
